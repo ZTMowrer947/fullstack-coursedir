d1f329f1d7d2ba830d1994987e050658ac3b0277
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-03 12:28:07 -0400

    Add "New Course" link

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -1,6 +1,7 @@
 // Imports
 import axios from "axios";
 import React from "react";
+import { Link } from "react-router-dom";
 import Course from "../components/Course";
 
 // Components
@@ -42,7 +43,23 @@ class Courses extends React.Component {
         return (
             <>
                 {courseList}
-                {/* TODO: Add "New course" link */}
+                <div className="grid-33">
+                    <Link to="/courses/create" className="course--module course--add--module">
+                        <h3 className="course--add--title">
+                            <svg
+                                version="1.1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                x="0px"
+                                y="0px"
+                                viewBox="0 0 13 13"
+                                className="add"
+                            >
+                                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                            </svg>
+                            New Course
+                        </h3>
+                    </Link>
+                </div>
             </>
         );
     }
