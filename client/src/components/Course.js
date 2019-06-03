b6f379d9acecc993a3564966a581008bff724629
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-03 17:50:37 -0400

    Add links to Course component

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -2,39 +2,54 @@
 import PropTypes from "prop-types";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
 
 // Components
 const Course = props => (
-    <div className="bounds course--detail">
-        <div className="grid-66">
-            <div className="course--header">
-                <h4 className="course--label">Course</h4>
-                <h3 className="course--title">{props.title}</h3>
-                <p>By {props.user.firstName} {props.user.lastName}</p>
-            </div>
-            <div className="course--description">
-                <ReactMarkdown source={props.description} />
-            </div>
-        </div>
-        <div className="grid-25 grid-right">
-            <div className="course--stats">
-                <ul className="course--stats--list">
-                    <li className="course--stats--list--item">
-                        <h4>Estimated Time</h4>
-                        <h3>{props.estimatedTime}</h3>
-                    </li>
-                    <li className="course--stats--list--item">
-                        <h4>Materials Needed</h4>
-                        <ReactMarkdown source={props.materialsNeeded} />
-                    </li>
-                </ul>
+    <>
+        <div className="actions--bar">
+            <div className="bounds">
+                <div className="grid-100">
+                    <span>
+                        <Link className="button" to={`/courses/${props.id}/update`}>Update Course</Link>
+                        <Link className="button" to={`/courses/${props.id}/delete`}>Delete Course</Link>
+                    </span>
+                    <Link className="button button-secondary" to="/">Return to List</Link>
+                </div>
             </div>
         </div>
-    </div>
+        <div className="bounds course--detail">
+            <div className="grid-66">
+                <div className="course--header">
+                    <h4 className="course--label">Course</h4>
+                    <h3 className="course--title">{props.title}</h3>
+                    <p>By {props.user.firstName} {props.user.lastName}</p>
+                </div>
+                <div className="course--description">
+                    <ReactMarkdown source={props.description} />
+                </div>
+            </div>
+            <div className="grid-25 grid-right">
+                <div className="course--stats">
+                    <ul className="course--stats--list">
+                        <li className="course--stats--list--item">
+                            <h4>Estimated Time</h4>
+                            <h3>{props.estimatedTime}</h3>
+                        </li>
+                        <li className="course--stats--list--item">
+                            <h4>Materials Needed</h4>
+                            <ReactMarkdown source={props.materialsNeeded} />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </>
 );
 
 // Prop Types
 Course.propTypes = {
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     estimatedTime: PropTypes.string.isRequired,
