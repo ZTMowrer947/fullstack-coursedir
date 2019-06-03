a50cdefbe74fc8fe96554be5d3b21bfa7de9402f
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-03 09:57:58 -0400

    Convert App component to function component

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,11 @@ import React from "react";
 import "./App.css";
 
 // Component
-class App extends React.Component {
-    // DOM rendering
-    render() {
-        return (
-            <div className="App">
-                <h1>It works!</h1>
-            </div>
-        );
-    }
-}
+const App = () => (
+    <div className="App">
+        <h1>It works!</h1>
+    </div>
+);
 
 // Export
 export default App;
