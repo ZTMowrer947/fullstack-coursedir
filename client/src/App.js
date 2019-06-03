7c3d6e44bd218dca64561aee8708d24c7d01e12b
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-03 09:53:10 -0400

    Modify imports to use double quotes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // Imports
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 // Component
 class App extends React.Component {
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,11 @@
 // Imports
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // Test Suite
-it('renders without crashing', () => {
-    const div = document.createElement('div');
+it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
