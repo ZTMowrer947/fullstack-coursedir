0edfe11d12f446badc6e15d5641e2e410214e756
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-03 09:23:21 -0400

    Modify client scripts to more suit my style

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,9 @@
+// Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Test Suite
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
+// Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Render app
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
