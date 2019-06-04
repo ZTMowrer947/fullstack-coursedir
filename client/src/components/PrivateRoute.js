af2d6fe50a254e0eb9c843f979dd3dcc563644be
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-04 15:18:49 -0400

    Add PrivateRoute component

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,28 @@
+// Imports
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+// Component
+const PrivateRoute = ({ component, ...rest }) => (
+    <Route {...rest} render={routeProps => (
+        // Get user from context
+        <AuthContext.Consumer>
+            {({ getCredentials, user }) => {
+                // Store component to potentially render
+                const Component = component;
+
+                // If a user is signed in,
+                if (user)
+                    // Render the provided component
+                    return <Component {...routeProps} user={user} getCredentials={getCredentials} />
+                else
+                    // Otherwise, redirect to the sign-in page
+                    return <Redirect to="/signin" />
+            }}
+        </AuthContext.Consumer>
+    )} />
+);
+
+// Export
+export default PrivateRoute;
