7497d08dad48d2e6023c04189e5db6c12bef76a2
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-07 14:10:05 -0400

    Add LoadingIndicator component

---
+++ client/src/components/LoadingIndicator.js
@@ -0,0 +1,26 @@
+// Imports
+import PropTypes from "prop-types";
+import React from "react";
+import { CircularProgress } from "@material-ui/core";
+
+// Component
+const LoadingIndicator = props => (
+    <div className="loading">
+        <CircularProgress size={props.size} />
+    </div>
+);
+
+// Prop Types
+LoadingIndicator.propTypes = {
+    size: PropTypes.oneOfType([
+        PropTypes.number.isRequired,
+        PropTypes.string.isRequired,
+    ]).isRequired,
+}
+
+LoadingIndicator.defaultProps = {
+    size: 80,
+}
+
+// Export
+export default LoadingIndicator;
