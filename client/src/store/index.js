5080c752f5fb05f530355bdb163ccadaffb627e3
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-28 10:28:13 -0400

    Modify Redux store to use root saga

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import initialState from "./initialState";
 import rootReducer from "./reducers";
-import courseSaga from "./sagas/courses";
+import rootSaga from "./sagas";
 
 // Saga Middleware
 const sagaMiddleware = createSagaMiddleware();
@@ -23,7 +23,7 @@ const store = createStore(
 );
 
 // Run Sagas
-sagaMiddleware.run(courseSaga);
+sagaMiddleware.run(rootSaga);
 
 // Export
 export default store;
