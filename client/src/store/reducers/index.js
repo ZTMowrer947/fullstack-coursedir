fb7c44eb28888047d527802f10e35b7cf97f445f
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-28 08:39:54 -0400

    Add basic root reducer

---
+++ client/src/store/reducers/index.js
@@ -0,0 +1,11 @@
+// Imports
+import courses from "./courses";
+import { combineReducers } from "redux";
+
+// Root reducer
+const rootReducer = combineReducers({
+    courses,
+});
+
+// Export
+export default rootReducer;
