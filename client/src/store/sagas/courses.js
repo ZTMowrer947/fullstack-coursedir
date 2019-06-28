225cad5d1ae3b0331a4f08c79a37980508a83e0d
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-28 09:52:17 -0400

    Update default export of course sagas

--- client/src/store/sagas/courses.js
+++ client/src/store/sagas/courses.js
@@ -1,5 +1,5 @@
 // Imports
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 import { types, coursesFetchDone } from "../actions/courses";
 import CourseService from "../../services/CourseService";
 
@@ -17,12 +17,7 @@ function* fetchCourses() {
     }
 }
 
-function* watchForCoursesFetch() {
+export default function* watchForCoursesFetch() {
     // Fetch courses when COURSES_FETCH_START is dispatched
-    yield takeLatest(types.COURSES_FETCH_DONE, fetchCourses);
-}
-
-export default function*() {
-    // Run all watcher sagas
-    yield all([watchForCoursesFetch]);
+    yield takeLatest(types.COURSES_FETCH_START, fetchCourses);
 }
