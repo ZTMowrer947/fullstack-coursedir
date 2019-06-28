e70a7e2bac5bfc1345dbfebadcccb2c1668b5bdf
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-28 08:56:26 -0400

    Add redux devtool support to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,7 @@ import rootReducer from "./reducers";
 import { createStore } from "redux";
 
 // Store
-const store = createStore(rootReducer, initialState, undefined);
+const store = createStore(rootReducer, initialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Export
 export default store;
