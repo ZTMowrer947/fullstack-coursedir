a29b6ce725e3ee52ba744d4bc1f3d1ab501467c4
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-28 09:25:09 -0400

    Add sagas for course list fetching

---
+++ client/src/store/sagas/courses.js
@@ -0,0 +1,28 @@
+// Imports
+import { all, call, put, takeLatest } from "redux-saga/effects";
+import { types, coursesFetchDone } from "../actions/courses";
+import CourseService from "../../services/CourseService";
+
+// Sagas
+function* fetchCourses() {
+    try {
+        // Get course listing
+        const courses = yield call(CourseService.getList);
+
+        // If successful, dispatch successful COURSES_FETCH_DONE action
+        yield put(coursesFetchDone(courses));
+    } catch (error) {
+        // If an error occurs, dispatch failed COURSES_FETCH_DONE action
+        yield put(coursesFetchDone(null, error));
+    }
+}
+
+function* watchForCoursesFetch() {
+    // Fetch courses when COURSES_FETCH_START is dispatched
+    yield takeLatest(types.COURSES_FETCH_DONE, fetchCourses);
+}
+
+export default function*() {
+    // Run all watcher sagas
+    yield all([watchForCoursesFetch]);
+}
