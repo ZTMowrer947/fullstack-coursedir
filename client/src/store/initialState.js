bc839da9c64cec31f029da67b929ade221cb08ac
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-29 13:24:59 -0400

    Update initialState credential initialization
    Retrieves credentials from cookie when initializing state

--- client/src/store/initialState.js
+++ client/src/store/initialState.js
@@ -1,10 +1,11 @@
 // Imports
 import { fromJS } from "immutable";
+import AuthService from "../services/AuthService";
 
 // State
 const initialState = {
     auth: {
-        credentials: null,
+        credentials: AuthService.getCredentials(),
         user: null,
         error: null,
         isFetching: false,
