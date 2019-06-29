1e4d503eb6ba72dd9a93a9f01c0d4cb713745363
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-29 15:52:40 -0400

    Connect Nav component to Redux

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,36 +1,50 @@
 // Imports
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import withImmutablePropsToJS from "with-immutable-props-to-js";
 
 // Component
-const Nav = props => (
-    /* Get auth service from context */
-    <AuthContext.Consumer>
-        {({ user }) => {
-            // If a user is signed in,
-            if (user) {
-                // Render welcome message and signout link
-                return (
-                    <nav>
-                        <span>
-                            Welcome {user.firstName} {user.lastName}!
-                        </span>
-                        <Link to="/signout" className="signout">Sign Out</Link>
-                    </nav>
-                );
-            } else {
-                // Otherwise, render links to sign-in and sign-up
-                return (
-                    <nav>
-                        <Link className="signup" to="/signup">Sign Up</Link>
-                        <Link className="signin" to={{ pathname: "/signin", state: { prevUrl: props.location.pathname } }}>Sign In</Link>
-                    </nav>
-                ) 
-            }
-        }}
-    </AuthContext.Consumer>
+const Nav = props => {
+    // If a user is signed in,
+    if (props.user) {
+        // Render welcome message and signout link
+        return (
+            <nav>
+                <span>
+                    Welcome {props.user.firstName} {props.user.lastName}!
+                </span>
+                <Link to="/signout" className="signout">Sign Out</Link>
+            </nav>
+        );
+    } else {
+        // Otherwise, render links to sign-in and sign-up
+        return (
+            <nav>
+                <Link className="signup" to="/signup">Sign Up</Link>
+                <Link className="signin" to={{ pathname: "/signin", state: { prevUrl: props.location.pathname } }}>Sign In</Link>
+            </nav>
+        );
+    }
+};
+
+// Redux mapping to React props
+const mapStateToProps = state => ({
+    user: state.getIn(["auth", "user"]),
+});
+
+// HOC Composition
+const enhance = compose(
+    // Add React Router props
+    withRouter,
+
+    // Connect to Redux
+    connect(mapStateToProps),
+
+    // Convert Immutable.js collections to plain JavaScript data
+    withImmutablePropsToJS,
 );
 
 // Export
-export default withRouter(Nav);
+export default enhance(Nav);
