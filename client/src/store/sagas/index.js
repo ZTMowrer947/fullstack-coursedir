520fba5c4f924d862936e73216dbea2608742418
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-29 12:27:46 -0400

    Add auth saga to root saga

--- client/src/store/sagas/index.js
+++ client/src/store/sagas/index.js
@@ -1,12 +1,13 @@
 // Imports
 import { all, spawn } from "redux-saga/effects";
+import authSaga from "./auth";
 import courseSaga from "./course";
 import coursesSaga from "./courses";
 
 // Root saga
 export default function* rootSaga() {
     // List of sagas to spawn
-    const sagas = [courseSaga, coursesSaga];
+    const sagas = [authSaga, courseSaga, coursesSaga];
 
     // Spawn each saga in parallel
     yield all(sagas.map(saga => spawn(saga)));
