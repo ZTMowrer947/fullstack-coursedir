42e46696ca3b22b04879c863d8c931604acadf5d
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-30 10:40:26 -0400

    Add form reducer to root reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -3,6 +3,7 @@ import auth from "./auth";
 import course from "./course";
 import courses from "./courses";
 import { connectRouter } from "connected-react-router";
+import { reducer as formReducer } from "redux-form/immutable";
 import { combineReducers } from "redux-immutable";
 
 // Root reducer creator
@@ -10,6 +11,7 @@ const createRootReducer = history => combineReducers({
     auth,
     course,
     courses,
+    form: formReducer,
     router: connectRouter(history),
 });
 
