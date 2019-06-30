abe91fa8f8e6ed9acdea070dfea6ebb78e6c1717
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-29 20:02:34 -0400

    Update PrivateRoute to use Redux state

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,31 +1,40 @@
 // Imports
 import React from "react";
+import { connect } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+import { compose } from "redux";
+import withImmutablePropsToJS from "with-immutable-props-to-js";
 
 // Component
-const PrivateRoute = ({ component, ...rest }) => (
-    <Route {...rest} render={routeProps => (
-        // Get user from context
-        <AuthContext.Consumer>
-            {({ getCredentials, user }) => {
-                // Store component to potentially render
-                const Component = component;
+const PrivateRoute = ({ component, credentials, user, ...rest }) => (
+    <Route {...rest} render={routeProps => {
+        // Store component to potentially render
+        const Component = component;
 
-                // If a user is signed in,
-                if (user)
-                    // Render the provided component
-                    return <Component {...routeProps} user={user} getCredentials={getCredentials} />
-                else
-                    // Otherwise, redirect to the sign-in page
-                    return <Redirect to={{
-                        pathname: "/signin",
-                        state: { prevUrl: rest.location.pathname }
-                    }} />
-            }}
-        </AuthContext.Consumer>
-    )} />
+        // If a user is signed in,
+        if (user)
+            // Render the provided component
+            return <Component {...routeProps} user={user} credentials={credentials} />
+        else
+            // Otherwise, redirect to the sign-in page
+            return <Redirect to={{
+                pathname: "/signin",
+                state: { prevUrl: rest.location.pathname }
+            }} />
+    }} />
 );
 
+// Redux mapping to React props
+const mapStateToProps = state => ({
+    credentials: state.getIn(["auth", "credentials"]),
+    user: state.getIn(["auth", "user"]),
+});
+
+// HOC composition
+const enhance = compose(
+    connect(mapStateToProps),
+    withImmutablePropsToJS,
+)
+
 // Export
-export default PrivateRoute;
+export default enhance(PrivateRoute);
