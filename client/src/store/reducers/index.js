7a8d160f5b4857378d6be3a9ab24c9c57124de2c
Zackery Mowrer <Zackmowrer@gmail.com>
2019-06-30 19:50:05 -0400

    Begrudgingly add back in redux-form

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,15 +1,17 @@
 // Imports
+import { connectRouter } from "connected-react-router";
+import { reducer as formReducer } from "redux-form/immutable";
+import { combineReducers } from "redux-immutable";
 import auth from "./auth";
 import course from "./course";
 import courses from "./courses";
-import { connectRouter } from "connected-react-router";
-import { combineReducers } from "redux-immutable";
 
 // Root reducer creator
 const createRootReducer = history => combineReducers({
     auth,
     course,
     courses,
+    form: formReducer,
     router: connectRouter(history),
 });
 
