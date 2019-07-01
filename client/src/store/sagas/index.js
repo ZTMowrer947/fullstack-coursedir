64576d3496ae609537339974fd18cbf6540ea437
Zackery Mowrer <Zackmowrer@gmail.com>
2019-07-01 19:00:18 -0400

    Add formActionSaga to root saga

--- client/src/store/sagas/index.js
+++ client/src/store/sagas/index.js
@@ -1,4 +1,5 @@
 // Imports
+import formActionSaga from "redux-form-saga";
 import { all, spawn } from "redux-saga/effects";
 import authSaga from "./auth";
 import courseSaga from "./course";
@@ -7,7 +8,7 @@ import coursesSaga from "./courses";
 // Root saga
 export default function* rootSaga() {
     // List of sagas to spawn
-    const sagas = [authSaga, courseSaga, coursesSaga];
+    const sagas = [authSaga, courseSaga, coursesSaga, formActionSaga];
 
     // Spawn each saga in parallel
     yield all(sagas.map(saga => spawn(saga)));
