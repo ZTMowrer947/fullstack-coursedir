ffe99855a460e261fbfbed1193058bf3b95bf526
Zackery Mowrer <Zackmowrer@gmail.com>
2019-07-01 19:57:37 -0400

    Implement submission management in redux

--- client/src/store/sagas/auth.js
+++ client/src/store/sagas/auth.js
@@ -1,8 +1,9 @@
 // Imports
 import { push } from "connected-react-router";
 import { setCookie, removeCookie } from "redux-cookie";
-import { all, cancel, cancelled, call, fork, put, spawn, take } from "redux-saga/effects";
-import { signInDone, types, resetSignInFlag, createUserDone, signInStart } from "../actions/auth";
+import { SubmissionError } from "redux-form";
+import { all, cancel, cancelled, call, fork, put, spawn, take, takeLatest } from "redux-saga/effects";
+import { signInDone, types, resetSignInFlag, signInStart, signUp } from "../actions/auth";
 import AuthService from "../../services/AuthService";
 import ValidationError from "../../ValidationError";
 
@@ -63,50 +64,83 @@ function* signInFlow() {
     }
 }
 
-function* createUser(userData) {
-    // If the password and confirm password don't match,
-    if (userData.password !== userData.confirmPassword) {
-        // Create error
-        const error = new ValidationError("Password and Confirm Password fields must match.", userData);
+function* createUser(action) {
+    // Get user data from action payload
+    const { payload: userData } = action;
 
-        // Dispatch failed CREATE_USER
-        yield put(createUserDone(error));
+    // If the password and confirm password don't match,
+    if (userData.get("password") !== userData.get("confirmPassword")) {
+        // Create error
+        const error = new SubmissionError({
+            _error: "Password and Confirm Password fields must match.",
+        }, userData);
+
+        // Dispatch submission failure
+        yield put(signUp.failure(error));
     } else {
         try {
             // Attempt to create user
             yield call(AuthService.createUser, userData);
     
-            // If successful, dispatch successful CREATE_USER_DONE
-            yield put(createUserDone());
+            // If successful, dispatch submission success
+            yield put(signUp.success());
     
             // Sign-in the created user
             yield put(signInStart(userData.emailAddress, userData.password, "/"));
         } catch (error) {
-            // If an error occurred, dispatch failed CREATE_USER_DONE
-            yield put(createUserDone(error));
+            // If a error is a validation error
+            if (error instanceof ValidationError) {
+                // Get validation error listing
+                const validationErrors = error.message.split(",")
+                    .map(error => error.substring(error.indexOf(": ") + 2))
+                    .reduce((validationErrors, error) => {
+                        // Declare variable to hold error key
+                        let key = error.split(" ", 1)[0];
+
+                        // If the error is not relating to the password,
+                        if (!error.startsWith("password")) {
+                            // Get second word in message
+                            let secondWord = error.split(" ")[1]
+
+                            // Capitalize word
+                            secondWord = secondWord[0].toUpperCase() + secondWord.slice(1);
+                            
+                            // Add to key name
+                            key += secondWord;
+                        }
+
+                        // Add key-value pair for validation error
+                        validationErrors[key] = error;
+
+                        return validationErrors;
+                    }, {});
+
+                // Create submission error
+                const submitError = new SubmissionError(validationErrors);
+                
+                // Dispatch submission failure
+                yield put(signUp.failure(submitError));
+            } else {
+                // Otherwise, create generic submission error
+                const submitError = new SubmissionError({
+                    _error: "An error occurred during user creation.",
+                });
+
+                // Dispatch submission failure
+                yield put(signUp.failure(submitError));
+            }
         }
     }
 }
 
-function* signUp() {
-    // Declare variable to hold previous task
-    let prevTask = null;
-
-    while (true) {
-        // Wait for COURSE_FETCH_START and get ID from payload
-        const { payload: userData } = yield take(types.CREATE_USER_START);
-
-        // If the previous task has been defined, cancel it if it isn't finished already
-        if (prevTask) yield cancel(prevTask);
-
-        // Retrieve course and store task for next run
-        prevTask = yield fork(createUser, userData);
-    }
+function* signUpFlow() {
+    // Watch for SIGN_UP_REQUEST
+    yield takeLatest(signUp.REQUEST, createUser);
 }
 
 export default function* authSaga() {
     // Define list of sagas to spawn
-    const sagas = [signInFlow, signUp];
+    const sagas = [signInFlow, signUpFlow];
 
     // Spawn sagas in parallel
     yield all(sagas.map(saga => spawn(saga)));
