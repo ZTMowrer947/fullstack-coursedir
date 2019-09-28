36b9e4cd0b71d8202a35497e42b5390492b646fa
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-28 07:54:08 -0400

    Modify MainNav component to use LinkContainers

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -2,20 +2,21 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import "./MainNav.scss";
+import { LinkContainer } from "react-router-bootstrap";
 
 // Component
 const MainNav: React.FC = () => {
     return (
         <Nav className="d-inline-flex justify-content-end align-items-center flex-grow-1">
             <Nav.Item>
-                <Nav.Link className="signup" href="/signup">
-                    Sign Up
-                </Nav.Link>
+                <LinkContainer to="/signup" exact>
+                    <Nav.Link className="signup">Sign Up</Nav.Link>
+                </LinkContainer>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link className="signin" href="/signin">
-                    Sign In
-                </Nav.Link>
+                <LinkContainer to="/signin" exact>
+                    <Nav.Link className="signin">Sign In</Nav.Link>
+                </LinkContainer>
             </Nav.Item>
         </Nav>
     );
