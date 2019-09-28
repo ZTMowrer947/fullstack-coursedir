1de93dcfc361e3660c0eeb9d0ccb7f73c19f1437
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-28 06:57:02 -0400

    Add Header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header";
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
+            <Header />
         </div>
     );
 };
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,15 @@
+// Imports
+import React from "react";
+import "./Header.scss";
+
+// Component
+const Header: React.FC = () => (
+    <header className="header">
+        <div className="bounds">
+            <h1 className="header-logo">Courses</h1>
+        </div>
+    </header>
+);
+
+// Export
+export default Header;
