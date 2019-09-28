b8062876546322fdd1740edec2c4c47781f25ebb
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-28 16:58:55 -0400

    Modify Course component styling to use Bootstrap

--- client/src/components/Course.tsx
+++ client/src/components/Course.tsx
@@ -1,5 +1,7 @@
 // Imports
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import ReactMarkdown from "react-markdown";
 import { Link } from "react-router-dom";
 import CourseModel from "../models/Course";
@@ -15,7 +17,7 @@ const Course: React.FC<PropTypes> = ({ course }) => {
         <>
             <div className="actions--bar">
                 <div className="bounds">
-                    <div className="grid-100">
+                    <div className="w-100">
                         <span>
                             <Link
                                 className="button"
@@ -37,42 +39,44 @@ const Course: React.FC<PropTypes> = ({ course }) => {
                 </div>
             </div>
             <div className="bounds course--detail">
-                <div className="grid-66">
-                    <div className="course--header">
-                        <h4 className="course--label">Course</h4>
-                        <h3 className="course--title">{course.title}</h3>
-                        <p>
-                            By {course.creator.firstName}{" "}
-                            {course.creator.lastName}
-                        </p>
-                    </div>
-                    <div className="course--description">
-                        <ReactMarkdown source={course.description} />
-                    </div>
-                </div>
-                <div className="grid-25 grid-right">
-                    <div className="course--stats">
-                        <ul className="course--stats--list">
-                            {/* Display estimated time only if data is present */}
-                            {course.estimatedTime && (
-                                <li className="course--stats--list--item">
-                                    <h4>Estimated Time</h4>
-                                    <h3>{course.estimatedTime}</h3>
-                                </li>
-                            )}
+                <Row>
+                    <Col xs={8}>
+                        <div className="course--header">
+                            <h4 className="course--label">Course</h4>
+                            <h3 className="course--title">{course.title}</h3>
+                            <p>
+                                By {course.creator.firstName}{" "}
+                                {course.creator.lastName}
+                            </p>
+                        </div>
+                        <div className="course--description">
+                            <ReactMarkdown source={course.description} />
+                        </div>
+                    </Col>
+                    <Col xs={3}>
+                        <div className="course--stats">
+                            <ul className="course--stats--list">
+                                {/* Display estimated time only if data is present */}
+                                {course.estimatedTime && (
+                                    <li className="course--stats--list--item">
+                                        <h4>Estimated Time</h4>
+                                        <h3>{course.estimatedTime}</h3>
+                                    </li>
+                                )}
 
-                            {/* Display needed materials only if data is present */}
-                            {course.materialsNeeded && (
-                                <li className="course--stats--list--item">
-                                    <h4>Materials Needed</h4>
-                                    <ReactMarkdown
-                                        source={course.materialsNeeded}
-                                    />
-                                </li>
-                            )}
-                        </ul>
-                    </div>
-                </div>
+                                {/* Display needed materials only if data is present */}
+                                {course.materialsNeeded && (
+                                    <li className="course--stats--list--item">
+                                        <h4>Materials Needed</h4>
+                                        <ReactMarkdown
+                                            source={course.materialsNeeded}
+                                        />
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
             </div>
         </>
     );
