167518d61e852d6102534363cb4ef5f850c7361e
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-27 22:16:41 -0400

    Add client prettierrc config

---
+++ client/.prettierrc.js
@@ -0,0 +1,17 @@
+// Prettier configuration
+module.exports = {
+    // Tab spacing
+    tabWidth: 4,
+
+    // Semicolons at the end of statements
+    semi: true,
+
+    // Use double quotes
+    singleQuote: false,
+
+    // Use trailing comma where valid in ECMAScript 5
+    trailingComma: "es5",
+
+    // Space brackets in object literals
+    bracketSpacing: true,
+};
