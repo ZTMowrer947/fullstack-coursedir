f7567a3a621d910cc16acdb1a663333269615f64
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-28 15:55:41 -0400

    Add CourseDetail page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.scss";
 import Layout from "./components/Layout";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Redirect, Route } from "react-router-dom";
 import Courses from "./components/pages/Courses";
+import CourseDetail from "./components/pages/CourseDetail";
 
 const App: React.FC = () => {
     return (
         <Layout>
             <Switch>
-                <Route path="/" exact component={Courses} />
+                <Redirect from="/" to="/courses" exact />
+                <Route path="/courses" exact component={Courses} />
+                <Route path="/courses/:id" exact component={CourseDetail} />
             </Switch>
         </Layout>
     );
