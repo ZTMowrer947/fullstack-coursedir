3d28dee57ed1962a8440dc90dd6c81b3cbca6583
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-29 12:05:44 -0400

    Modify MainNav to show signout link when signed in

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -1,23 +1,44 @@
 // Imports
-import React from "react";
+import React, { useContext } from "react";
 import Nav from "react-bootstrap/Nav";
-import "./MainNav.scss";
 import { LinkContainer } from "react-router-bootstrap";
+import AuthContext from "../context/AuthContext";
+import "./MainNav.scss";
 
 // Component
 const MainNav: React.FC = () => {
+    // Get user from context
+    const { user } = useContext(AuthContext);
+
     return (
         <Nav className="d-inline-flex justify-content-end align-items-center flex-grow-1">
-            <Nav.Item>
-                <LinkContainer to="/signup" exact>
-                    <Nav.Link className="signup">Sign Up</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
-            <Nav.Item>
-                <LinkContainer to="/signin" exact>
-                    <Nav.Link className="signin">Sign In</Nav.Link>
-                </LinkContainer>
-            </Nav.Item>
+            {!!user ? (
+                <>
+                    <Nav.Item>
+                        <span>
+                            Welcome {`${user.firstName} ${user.lastName}`}!
+                        </span>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <LinkContainer to="/signout" exact>
+                            <Nav.Link className="signout">Sign Out</Nav.Link>
+                        </LinkContainer>
+                    </Nav.Item>
+                </>
+            ) : (
+                <>
+                    <Nav.Item>
+                        <LinkContainer to="/signup" exact>
+                            <Nav.Link className="signup">Sign Up</Nav.Link>
+                        </LinkContainer>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <LinkContainer to="/signin" exact>
+                            <Nav.Link className="signin">Sign In</Nav.Link>
+                        </LinkContainer>
+                    </Nav.Item>
+                </>
+            )}
         </Nav>
     );
 };
