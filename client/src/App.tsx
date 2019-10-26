118decd2a022619e62c185ad9e3c4c0cffe8462c
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-26 15:59:27 -0400

    Rewrite App component as component class

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,112 +1,156 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import LoadingIndicator from "./components/LoadingIndicator";
+import PrivateRoute from "./components/PrivateRoute";
 import AuthContext from "./context/AuthContext";
 import AuthState from "./models/AuthState";
-import User from "./models/User";
 import UserService from "./services/User.service";
 import "./App.scss";
 
-// Async Components
+// Dynamically Imported Components
 const Courses = React.lazy(() => import("./components/pages/Courses"));
 const CourseDetail = React.lazy(() =>
     import("./components/pages/CourseDetail")
+);
+const CreateCourse = React.lazy(() =>
+    import("./components/pages/CreateCourse")
 );
 const UserSignIn = React.lazy(() => import("./components/pages/UserSignIn"));
 const UserSignUp = React.lazy(() => import("./components/pages/UserSignUp"));
 const UserSignOut = React.lazy(() => import("./components/pages/UserSignOut"));
 
-const App: React.FC = () => {
-    // Initialize state
-    const [user, setUser] = useState<User | undefined>(undefined);
-    const [loading, setLoading] = useState(true);
+// State type
+interface AppState {
+    readonly auth: Readonly<AuthState>;
+}
 
-    // Construct signIn and signOut callback
-    const signIn = useCallback(
-        async (emailAddress: string, password: string) => {
-            // Attempt to sign in user
-            const authUser = await UserService.signIn(emailAddress, password);
+// Component
+class App extends React.Component<{}, AppState> {
+    public constructor(props: {}) {
+        super(props);
 
-            // Set user state
-            setUser(authUser);
-        },
-        [setUser]
-    );
+        this.state = {
+            auth: {
+                loading: true,
+                user: undefined,
+                getCredentials: UserService.getCredentials,
+                signIn: this.signIn.bind(this),
+                signUp: UserService.signUp,
+                signOut: this.signOut.bind(this),
+            },
+        };
+    }
 
-    const signOut = useCallback(() => {
+    public async signIn(emailAddress: string, password: string): Promise<void> {
+        // Sign in user
+        const user = await UserService.signIn(emailAddress, password);
+
+        // Attach user to state
+        this.setState(prevState => ({
+            ...prevState,
+            auth: {
+                ...prevState.auth,
+                user,
+            },
+        }));
+    }
+
+    public signOut(): void {
         // Sign out user
         UserService.signOut();
 
-        // Clear user state
-        setUser(undefined);
-    }, [setUser]);
+        // Remove user from state
+        this.setState(prevState => ({
+            ...prevState,
+            auth: {
+                ...prevState.auth,
+                user: undefined,
+            },
+        }));
+    }
 
-    // Construct AuthState
-    const authState = useMemo<AuthState>(
-        () => ({
-            user,
-            loading,
-            getCredentials: UserService.getCredentials,
-            signIn,
-            signUp: UserService.signUp,
-            signOut,
-        }),
-        [signIn, signOut, user, loading]
-    );
-
-    useEffect(() => {
+    public componentDidMount() {
         // Attempt to get credentials from cookie data
-        const credentials = authState.getCredentials();
+        const credentials = this.state.auth.getCredentials();
 
-        // If there are persisted credentials, but the user hasn't been signed in,
-        // and we are not currently loading,
-        if (!!credentials && !user && !loading) {
+        // Get user from auth state
+        const user = this.state.auth.user;
+
+        // If credentials are present, but the user data is not,
+        if (!!credentials && !user) {
             // Decode credentials
             const decodedCredentials = atob(credentials);
             const [emailAddress, password] = decodedCredentials.split(":");
 
             // Sign in user
-            authState
+            this.state.auth
                 .signIn(emailAddress, password)
                 .catch(() => {
                     // If credentials were invalid, sign out user
-                    authState.signOut();
+                    this.state.auth.signOut();
                 })
                 .finally(() => {
-                    // In any case, unset loading flag
-                    setLoading(false);
+                    // Whether successful or not, unset loading flag
+                    this.setState(prevState => ({
+                        ...prevState,
+                        auth: {
+                            ...prevState.auth,
+                            loading: false,
+                        },
+                    }));
                 });
         } else {
-            // Otherwise, just unset loading flag
-            setLoading(false);
+            // Otherwise, unset loading flag
+            this.setState(prevState => ({
+                ...prevState,
+                auth: {
+                    ...prevState.auth,
+                    loading: false,
+                },
+            }));
         }
-    }, [authState, user, loading]);
+    }
 
-    return (
-        <AuthContext.Provider value={authState}>
-            <Layout>
-                {loading ? (
-                    <LoadingIndicator />
-                ) : (
-                    <React.Suspense fallback={<LoadingIndicator />}>
-                        <Switch>
-                            <Redirect from="/" to="/courses" exact />
-                            <Route path="/courses" exact component={Courses} />
-                            <Route
-                                path="/courses/:id"
-                                exact
-                                component={CourseDetail}
-                            />
-                            <Route path="/signin" component={UserSignIn} />
-                            <Route path="/signup" component={UserSignUp} />
-                            <Route path="/signout" component={UserSignOut} />
-                        </Switch>
-                    </React.Suspense>
-                )}
-            </Layout>
-        </AuthContext.Provider>
-    );
-};
+    public render() {
+        return (
+            <AuthContext.Provider value={this.state.auth}>
+                <Layout>
+                    {this.state.auth.loading ? (
+                        <LoadingIndicator />
+                    ) : (
+                        <React.Suspense fallback={<LoadingIndicator />}>
+                            <Switch>
+                                <Redirect from="/" to="/courses" exact />
+                                <Route
+                                    path="/courses"
+                                    exact
+                                    component={Courses}
+                                />
+                                <PrivateRoute
+                                    path="/courses/create"
+                                    exact
+                                    component={CreateCourse}
+                                />
+                                <Route
+                                    path="/courses/:id"
+                                    exact
+                                    component={CourseDetail}
+                                />
+                                <Route path="/signin" component={UserSignIn} />
+                                <Route path="/signup" component={UserSignUp} />
+                                <Route
+                                    path="/signout"
+                                    component={UserSignOut}
+                                />
+                            </Switch>
+                        </React.Suspense>
+                    )}
+                </Layout>
+            </AuthContext.Provider>
+        );
+    }
+}
 
+// Export
 export default App;
