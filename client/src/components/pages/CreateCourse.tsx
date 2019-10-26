63d9166a1fdac356dbaddb810ffda16a01dd5e10
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-26 15:59:16 -0400

    Add PrivateRoute and CreateCourse components

---
+++ client/src/components/pages/CreateCourse.tsx
@@ -0,0 +1,14 @@
+// Imports
+import React from "react";
+
+// Component
+const CreateCourse: React.FC = () => {
+    return (
+        <div className="bounds course--detail">
+            <h1>Create Course</h1>
+        </div>
+    );
+};
+
+// Export
+export default CreateCourse;
