19e73e9eef8f837f00fd0f9cda8a79f29c5c0f63
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-27 11:48:18 -0400

    Correct API url in CourseService.create

--- client/src/services/Course.service.ts
+++ client/src/services/Course.service.ts
@@ -29,7 +29,7 @@ export default class CourseService {
 
         // Make API request
         const response = await axios.post(
-            "http://localhost:5000/courses",
+            "http://localhost:5000/api/courses",
             courseData,
             {
                 auth: {
