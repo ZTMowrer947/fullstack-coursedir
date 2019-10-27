5e68647e49cb31188afcf4a4a5d3dfc3346753c9
Zackery Mowrer <Zackmowrer@gmail.com>
2019-10-27 11:32:19 -0400

    Add create method to CourseService

---
+++ client/src/models/ModifyCourseDTO.ts
@@ -0,0 +1,10 @@
+// Data Transfer Object
+interface ModifyCourseDTO {
+    title: string;
+    description: string;
+    estimatedTime?: string;
+    materialsNeeded?: string;
+}
+
+// Export
+export default ModifyCourseDTO;
--- client/src/services/Course.service.ts
+++ client/src/services/Course.service.ts
@@ -1,6 +1,7 @@
 // Imports
 import axios from "axios";
 import Course from "../models/Course";
+import ModifyCourseDTO from "../models/ModifyCourseDTO";
 
 // Service
 export default class CourseService {
@@ -16,5 +17,36 @@ export default class CourseService {
         );
 
         return response.data;
+    }
+
+    public static async create(
+        credentials: string,
+        courseData: ModifyCourseDTO
+    ): Promise<string> {
+        // Decode credentials
+        const decodedCredentials = atob(credentials);
+        const [emailAddress, password] = decodedCredentials.split(":");
+
+        // Make API request
+        const response = await axios.post(
+            "http://localhost:5000/courses",
+            courseData,
+            {
+                auth: {
+                    username: emailAddress,
+                    password,
+                },
+            }
+        );
+
+        // Get location from response headers
+        const location: string = response.headers.location;
+
+        // Extract course id from location
+        const lastSlashIndex = location.lastIndexOf("/");
+        const courseId = location.substring(lastSlashIndex + 1);
+
+        // Return the course id
+        return courseId;
     }
 }
