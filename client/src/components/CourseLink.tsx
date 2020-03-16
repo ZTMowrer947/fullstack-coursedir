ff8c63e8d04c7e16d3bfa226d1d56908fff8740f
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-16 19:07:28 -0400

    Add Course type and CourseLink component

---
+++ client/src/components/CourseLink.tsx
@@ -0,0 +1,27 @@
+// Imports
+import React from "react";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
+
+import Course from "../models/Course";
+
+// Prop Types
+interface PropTypes {
+    course: Course;
+}
+
+// Component
+const CourseLink: React.FC<PropTypes> = ({ course }) => (
+    <Col xs={4}>
+        <Link
+            to={`/courses/${course.id}`}
+            className="course-module course-link"
+        >
+            <h4 className="course-label">Course</h4>
+            <h3 className="course-title">{course.title}</h3>
+        </Link>
+    </Col>
+);
+
+// Export
+export default CourseLink;
---
+++ client/src/models/Course.ts
@@ -0,0 +1,11 @@
+// Type
+interface Course {
+    id: string;
+    title: string;
+    description: string;
+    estimatedTime?: string;
+    materialsNeeded?: string;
+}
+
+// Export
+export default Course;
