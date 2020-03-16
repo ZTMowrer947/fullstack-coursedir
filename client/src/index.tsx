fc4875b325557272f5c816809c79fff51fb5a66e
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-16 17:04:50 -0400

    Add BrowserRouter around App component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 // Imports
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -8,7 +9,12 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.scss";
 
 // Render app
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
