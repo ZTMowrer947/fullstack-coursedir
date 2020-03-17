3351527274a718731639548cd7c153364e7bebf1
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-17 15:07:27 -0400

    Add courses page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
 // Imports
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 
 import './App.scss';
+import Courses from './components/pages/Courses';
 
 // Component
-const App: React.FC = () => <Header />;
+const App: React.FC = () => (
+    <>
+        <Header />
+        <Container fluid>
+            <Switch>
+                <Redirect from="/" to="/courses" exact />
+                <Route path="/courses" exact component={Courses} />
+            </Switch>
+        </Container>
+    </>
+);
 
 // Export
 export default App;
