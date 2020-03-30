cf9abafd969f0ce323999894d9ddf68a7fa843c8
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-30 12:05:43 -0400

    Add user logon test to Header component tests

--- client/src/components/Header.spec.tsx
+++ client/src/components/Header.spec.tsx
@@ -4,10 +4,13 @@ import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 
 import Header from './Header';
+import AuthState from '../models/AuthState';
+import AuthContext from '../context/AuthContext';
+import UserFaker from '../services/__testutils__/UserFaker';
 
 // Test Suite
 describe('Header component', () => {
-    it('should render signup and signin links', () => {
+    it('should render signup and signin links when user is logged out', () => {
         // Render component
         const { getByTestId } = render(
             <StaticRouter>
@@ -22,5 +25,40 @@ describe('Header component', () => {
         // Expect link hrefs to link to correct pages
         expect(signupLink).toHaveAttribute('href', '/signup');
         expect(signinLink).toHaveAttribute('href', '/signin');
+    });
+
+    it('should render greeting and signout link when user is logged in', () => {
+        // Generate random user
+        const user = UserFaker.fakeUser();
+
+        // Setup context value
+        const contextValue: AuthState = {
+            user,
+            getCredentials: () => undefined,
+            signIn: async () => {},
+            signOut: () => {},
+        };
+
+        // Render component
+        const { getByTestId } = render(
+            <AuthContext.Provider value={contextValue}>
+                <StaticRouter>
+                    <Header />
+                </StaticRouter>
+            </AuthContext.Provider>
+        );
+
+        // Get welcome message and signin link
+        const greeting = getByTestId('greeting');
+        const signoutLink = getByTestId('signout');
+
+        // Define expected greeting text
+        const greetingText = `Welcome ${user.firstName} ${user.lastName}!`;
+
+        // Expect greeting to contain expected text
+        expect(greeting).toHaveTextContent(greetingText);
+
+        // Expect signout link href to link to signout route
+        expect(signoutLink).toHaveAttribute('href', '/signout');
     });
 });
