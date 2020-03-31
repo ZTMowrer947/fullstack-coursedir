e94dfcbcde1a353c6e6aa8fc2052b3fdd872464a
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-31 18:37:56 -0400

    Apply error abstraction to CourseApi

--- client/src/services/CourseApi.ts
+++ client/src/services/CourseApi.ts
@@ -2,15 +2,21 @@
 import axios, { AxiosError } from 'axios';
 
 import Course from '../models/Course';
+import { UnexpectedServerError } from '../models/errors';
 
 // API Service
 export default class CourseApi {
     public static async getList() {
-        // Make request to API
-        const response = await axios.get<Course[]>('/api/courses');
+        try {
+            // Make request to API
+            const response = await axios.get<Course[]>('/api/courses');
 
-        // Return response data
-        return response.data;
+            // Return response data
+            return response.data;
+        } catch (error) {
+            // If any error occurs, throw UnexpectedServerError
+            throw new UnexpectedServerError();
+        }
     }
 
     public static async get(id: string) {
@@ -24,17 +30,14 @@ export default class CourseApi {
             // Cast error as AxiosError
             const axiosError = error as AxiosError;
 
-            // If the error is not an axios error, or not a 404 error,
-            if (
-                !axiosError.isAxiosError ||
-                axiosError.response?.status !== 404
-            ) {
-                // Rethrow error
-                throw error;
-            }
+            // If the error is not an axios error, rethrow error
+            if (!axiosError.isAxiosError) throw error;
 
-            // Otherwise, return undefined
-            return undefined;
+            // If response status is 404, return undefined
+            if (axiosError.response?.status === 404) return undefined;
+
+            // Otherwise, throw UnexpectedServerError
+            throw new UnexpectedServerError();
         }
     }
 }
