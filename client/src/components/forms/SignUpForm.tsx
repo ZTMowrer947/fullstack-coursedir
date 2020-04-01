f86598be09a2f591076de3b2b1221289a58779b4
Zackery Mowrer <Zackmowrer@gmail.com>
2020-03-31 20:07:09 -0400

    Fully implement SignUpForm using Formik

--- client/src/components/forms/SignUpForm.tsx
+++ client/src/components/forms/SignUpForm.tsx
@@ -23,7 +23,7 @@ interface PropTypes {
 }
 
 // Form Component
-const SignUpForm: React.FC<PropTypes> = () => {
+const SignUpForm: React.FC<PropTypes> = ({ onSubmit }) => {
     // Define initial values
     const initialValues: SignUpFormValues = {
         firstName: '',
@@ -34,56 +34,113 @@ const SignUpForm: React.FC<PropTypes> = () => {
     };
 
     return (
-        <Form noValidate method="POST">
-            <Form.Group>
-                <Form.Control
-                    type="text"
-                    id="firstName"
-                    name="firstName"
-                    placeholder="First Name"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                    type="text"
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Last Name"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                    type="email"
-                    id="emailAddress"
-                    name="emailAddress"
-                    placeholder="Email Address"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Control
-                    type="password"
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                />
-            </Form.Group>
-            <Form.Group>
-                <Button className="mr-3" variant="primary" type="submit">
-                    Sign Up
-                </Button>
-                <LinkContainer exact to="/">
-                    <Button variant="outline-primary">Cancel</Button>
-                </LinkContainer>
-            </Form.Group>
-        </Form>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            {({
+                errors,
+                isSubmitting,
+                touched,
+                values,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+            }) => (
+                <Form noValidate method="POST" onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Control
+                            type="text"
+                            id="firstName"
+                            name="firstName"
+                            placeholder="First Name"
+                            value={values.firstName}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            isInvalid={touched.firstName && !!errors.firstName}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.firstName}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="text"
+                            id="lastName"
+                            name="lastName"
+                            placeholder="Last Name"
+                            value={values.lastName}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            isInvalid={touched.lastName && !!errors.lastName}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.lastName}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="email"
+                            id="emailAddress"
+                            name="emailAddress"
+                            placeholder="Email Address"
+                            value={values.emailAddress}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            isInvalid={
+                                touched.emailAddress && !!errors.emailAddress
+                            }
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.emailAddress}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={values.password}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            isInvalid={touched.password && !!errors.password}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.password}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Confirm Password"
+                            value={values.confirmPassword}
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            isInvalid={
+                                touched.confirmPassword &&
+                                !!errors.confirmPassword
+                            }
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.confirmPassword}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group>
+                        <Button
+                            className="mr-3"
+                            variant="primary"
+                            type="submit"
+                            disabled={isSubmitting}
+                        >
+                            Sign Up
+                        </Button>
+                        <LinkContainer exact to="/">
+                            <Button variant="outline-primary">Cancel</Button>
+                        </LinkContainer>
+                    </Form.Group>
+                </Form>
+            )}
+        </Formik>
     );
 };
 
