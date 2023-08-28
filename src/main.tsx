f1d9a03a9ae7f87529acf1cfd6c895482b7eec35
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-28 10:54:07 -0400

    Add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.tsx';
 
 // Initialize mock service worker in dev environment
 if (import.meta.env.DEV) {
@@ -12,6 +13,6 @@ if (import.meta.env.DEV) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
