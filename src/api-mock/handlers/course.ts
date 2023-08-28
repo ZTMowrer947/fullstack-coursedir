969cc147ef91f023c40ed57aa5017e4672bc96f2
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-28 10:01:34 -0400

    Add placeholder mock API routes

---
+++ src/api-mock/handlers/course.ts
@@ -0,0 +1,22 @@
+import { rest } from 'msw';
+import { apiBaseUrl } from '../../config.ts';
+
+export const courseListing = rest.get(`${apiBaseUrl}/api/courses`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
+
+export const newCourse = rest.post(`${apiBaseUrl}/api/courses`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
+
+export const courseDetail = rest.get(`${apiBaseUrl}/api/courses/:id`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
+
+export const updateCourse = rest.put(`${apiBaseUrl}/api/courses/:id`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
+
+export const deleteCourse = rest.delete(`${apiBaseUrl}/api/courses/:id`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
--- src/api-mock/handlers/user.ts
+++ src/api-mock/handlers/user.ts
@@ -1,5 +1,10 @@
 import { rest } from 'msw';
+import { apiBaseUrl } from '../../config.ts';
 
-export const getUser = rest.get('http://localhost:3000/api/users', (_req, res, ctx) => {
+export const getUser = rest.get(`${apiBaseUrl}/api/users`, (_req, res, ctx) => {
+  return res(ctx.status(503));
+});
+
+export const newUser = rest.post(`${apiBaseUrl}/api/users`, (_req, res, ctx) => {
   return res(ctx.status(503));
 });
