a7ad1180fd500bf5c737c19a0900dc53fff39e9f
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-28 08:05:05 -0400

    Add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/old/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
+  },
+});
