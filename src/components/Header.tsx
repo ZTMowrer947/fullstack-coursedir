e008c80b43bd58c80454702e0316634e8b85a08e
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-28 12:01:27 -0400

    Style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,15 @@
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <div className="bounds">
-        <div className="header--logo">
-          <h1>Courses</h1>
-          <nav>
-            <Link to={`signin`} className="signin">
-              Sign in
-            </Link>
-            <Link to={`signup`} className="signup">
-              Sign up
-            </Link>
-          </nav>
-        </div>
-      </div>
+    <header className={styles.header}>
+      <h1 className={styles.logo}>Courses</h1>
+
+      <nav className={styles.nav}>
+        <Link to={`signin`}>Sign in</Link>
+        <Link to={`signup`}>Sign up</Link>
+      </nav>
     </header>
   );
 }
