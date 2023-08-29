6fa1f9aacaba165e8ba2c99bfadf93f215c9a662
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-29 11:21:22 -0400

    Adjust MSW preparation

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,20 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
-// Initialize mock service worker in dev environment
-if (import.meta.env.DEV) {
-  const { default: worker } = await import('./api-mock/browser.ts');
+async function prepare() {
+  // Initialize mock service worker in dev environment
+  if (import.meta.env.DEV) {
+    await import('../public/mockServiceWorker.js?worker');
 
-  await worker.start();
+    const { default: worker } = await import('./api-mock/browser.ts');
+
+    return worker.start();
+  }
+
+  return Promise.resolve();
 }
+
+await prepare();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
