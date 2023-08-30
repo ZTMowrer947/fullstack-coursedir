ca6a735fb79b2031843abb88d21f67e7cfb09765
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2023-08-30 17:48:51 -0400

    Setup @ alias for src dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
     exclude: [
