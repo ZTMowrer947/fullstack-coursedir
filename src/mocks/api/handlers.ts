a29b862c3ec741b3f3f2912a46d12710e4e70de4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-01-16 12:56:10 -0600

    Add mock course preview listing route

--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,5 +1,13 @@
-import { RequestHandler } from 'msw';
+import { HttpResponse, RequestHandler, http } from 'msw';
+import db from './db';
 
-const handlers: RequestHandler[] = [];
+const handlers: RequestHandler[] = [
+  http.get('/api/courses', () => {
+    // Get courses and map into previews
+    const courses = db.course.findMany({}).map(({ id, title }) => ({ id, title }));
+
+    return HttpResponse.json(courses);
+  }),
+];
 
 export default handlers;
