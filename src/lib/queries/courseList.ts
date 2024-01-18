1f587251fc676e12cd1ba592ce4802c71fd97c2c
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-01-18 11:50:41 -0600

    Add courseList query

---
+++ src/lib/queries/courseList.ts
@@ -0,0 +1,21 @@
+import type { QueryKey } from '@tanstack/react-query';
+import type { CoursePreview } from '../../entities/course';
+
+const queryKey: QueryKey = ['courses'];
+
+async function fetchCourseList(): Promise<CoursePreview[]> {
+  const res = await fetch('/api/courses');
+
+  if (!res.ok)
+    // TODO: Add more robust error type and message
+    throw new Error(`Unexpected error fetching course listing`);
+
+  return res.json();
+}
+
+export default fetchCourseList;
+
+export const courseListQuery = {
+  queryKey,
+  queryFn: fetchCourseList,
+};
