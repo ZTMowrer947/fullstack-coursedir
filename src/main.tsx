42efe7ba185444c186130370448acbd2297bb10a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-01-18 11:14:34 -0600

    Setup mock db seeding

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,13 @@ if (import.meta.env.DEV) {
   const { worker } = await import('./mocks/api/browser.ts');
 
   await worker.start();
+
+  // Seed mock database if needed
+  const { default: mockDb, seedDb } = await import('./mocks/api/db.ts');
+
+  if (mockDb.course.count() === 0 && mockDb.user.count() === 0) {
+    seedDb();
+  }
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
