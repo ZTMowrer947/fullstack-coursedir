eaf1ed2f749cb666e24165018dd6cf90910e8297
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-01-18 11:46:11 -0600

    Add query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 import routes from './pages/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useRef } from 'react';
 
 function App() {
+  const queryClientRef = useRef(new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClientRef.current}>
       <div>
         <RouterProvider router={routes} />
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
