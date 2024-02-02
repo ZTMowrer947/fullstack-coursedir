185cd484fd0e68f1155f6d0ada2569cfe920c06a
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-02 14:08:44 -0600

    Add user info query and backend

---
+++ src/lib/queries/userInfo.ts
@@ -0,0 +1,36 @@
+import { User } from '../../entities/user';
+
+export interface UserCredentials {
+  emailAddress: string;
+  password: string;
+}
+
+async function fetchUserInfo({ emailAddress, password }: UserCredentials): Promise<User | null> {
+  const credentials = btoa(`${emailAddress}:${password}`);
+
+  const res = await fetch('/api/users', {
+    headers: {
+      authorization: `Basic ${credentials}`,
+      accept: 'application/json',
+    },
+  });
+
+  if (res.ok) {
+    const user = await res.json();
+
+    return user;
+  } else if (res.status == 401) {
+    return null;
+  } else {
+    throw new Error('Unexpected failure fetching user data');
+  }
+}
+
+export default fetchUserInfo;
+
+export function userInfoQuery(credentials: UserCredentials) {
+  return {
+    queryKey: ['user'] as const,
+    queryFn: () => fetchUserInfo(credentials),
+  };
+}
--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,6 +1,7 @@
-import { HttpResponse, RequestHandler, http } from 'msw';
+import { HttpResponse, RequestHandler, StrictResponse, http } from 'msw';
 import db from './db';
 import { Course } from '../../entities/course';
+import { User } from '../../entities/user';
 
 const handlers: RequestHandler[] = [
   http.get('/api/courses', () => {
@@ -42,6 +43,87 @@ const handlers: RequestHandler[] = [
     } else {
       return HttpResponse.json({ message: 'not found' }, { status: 401 });
     }
+  }),
+
+  http.get('/api/user', ({ request }): StrictResponse<{ message: string } | User> => {
+    const authHeader = request.headers.get('authorization');
+    const hdrRegex = /^Basic (?<credentials>[A-Za-z0-9+/=]+)$/;
+
+    // Ensure Basic auth is being used
+    if (!authHeader?.startsWith('Basic')) {
+      return HttpResponse.json(
+        { message: 'Auth not provided' },
+        {
+          status: 401,
+          headers: {
+            'www-authenticate': 'Basic',
+          },
+        },
+      );
+    }
+
+    const result = hdrRegex.exec(authHeader);
+
+    // Ensure credentials exist within header
+    if (!result?.groups?.credentials) {
+      return HttpResponse.json(
+        { message: 'Invalid auth type provided' },
+        {
+          status: 401,
+          headers: {
+            'www-authenticate': 'Basic',
+          },
+        },
+      );
+    }
+
+    // Decode credentials
+    const credStr = atob(result.groups.credentials);
+    const credParts = credStr.split(':');
+
+    // If credentials are not in proper format, return 401
+    if (credParts.length < 2) {
+      return HttpResponse.json(
+        { message: 'Invalid credential format' },
+        {
+          status: 401,
+          headers: {
+            'www-authenticate': 'Basic',
+          },
+        },
+      );
+    }
+
+    // Extract email and password from credentials
+    const [emailAddress, ...rest] = credParts;
+    const password = rest.join(':'); // To account for a password containing a colon
+
+    // Attempt to find user with credentials
+    const user = db.user.findFirst({
+      where: {
+        emailAddress: {
+          equals: emailAddress,
+        },
+        password: {
+          equals: password,
+        },
+      },
+    });
+
+    // If not found, return 401
+    if (!user) {
+      return HttpResponse.json({ message: 'Incorrect credentials ' }, { status: 401 });
+    }
+
+    // Extract only the desired properties of the user
+    const userData: User = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+    };
+
+    return HttpResponse.json(userData);
   }),
 ];
 
