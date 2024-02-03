a3f7d180c0394d106e4a28efe08fbe8b5075fb6d
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-03 08:24:13 -0600

    Implement form controls for signup form

--- src/pages/(auth)/signup/page.tsx
+++ src/pages/(auth)/signup/page.tsx
@@ -1,30 +1,58 @@
-import { Link } from 'react-router-dom';
+import { Link, useSubmit } from 'react-router-dom';
+import type { SignUpFormData } from '../../../lib/mutations/newUser';
+import { useForm } from 'react-hook-form';
+import { FormEventHandler } from 'react';
 
 export default function SignupPage() {
+  const submit = useSubmit();
+  const { register, trigger, getValues } = useForm<SignUpFormData>();
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    // Perform form validation
+    const isValid = await trigger();
+    event.preventDefault();
+
+    // If validation succeeded, proceed with submission
+    if (isValid) {
+      // Send form data to action as JSON
+      const data = getValues();
+
+      submit(JSON.stringify(data), {
+        method: 'post',
+        encType: 'application/json',
+      });
+    }
+  };
+
   return (
-    <form action="#" method="post">
+    <form action="#" method="post" onSubmit={handleFormSubmit}>
       <fieldset>
         <div>
           <label htmlFor="firstName">First Name</label>
-          <input type="text" name="firstName" id="firstName" placeholder="First Name..." />
+          <input type="text" id="firstName" placeholder="First Name..." {...register('firstName')} />
         </div>
         <div>
           <label htmlFor="lastname">Last Name</label>
-          <input type="text" name="lastname" id="lastName" placeholder="Last Name..." />
+          <input type="text" id="lastName" placeholder="Last Name..." {...register('lastName')} />
         </div>
         <div>
           <label htmlFor="emailAddress">Email Address</label>
-          <input type="email" name="emailAddress" id="emailAddress" placeholder="Email Address..." />
+          <input type="email" id="emailAddress" placeholder="Email Address..." {...register('emailAddress')} />
         </div>
       </fieldset>
       <fieldset>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="Password..." />
+          <input type="password" id="password" placeholder="Password..." {...register('password')} />
         </div>
         <div>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm Password..." />
+          <input
+            type="password"
+            id="confirmPassword"
+            placeholder="Confirm Password..."
+            {...register('confirmPassword')}
+          />
         </div>
       </fieldset>
       <div>
