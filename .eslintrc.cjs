41a45a6f74fdaed24e51afc727b5af6034708dde
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-03 08:42:02 -0600

    Add react and import sort plugins to eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,27 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    linkComponents: ['Link', 'NavLink'],
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'prettier'
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'sort-imports': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
   },
-}
+};
