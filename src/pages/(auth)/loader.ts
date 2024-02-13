4efad28515a6585738263c2932ff9d2c85fbae88
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-13 10:15:16 -0600

    Add logic to remove cookie if credentials are invalid
    Add more comments to auth loader

--- src/pages/(auth)/loader.ts
+++ src/pages/(auth)/loader.ts
@@ -9,19 +9,34 @@ const authLoader = (queryClient: QueryClient) => {
   return async () => {
     const credentials = Cookies.get('sdbc-credentials');
 
+    // No credentials, no user
     if (!credentials) {
       return defer({
         user: null,
       });
     }
 
+    // Fetch user using credentials
     // TODO: Add more thorough validation of valid user credentials
     const [emailAddress, password] = atob(credentials).split(':');
 
     const query = userInfoQuery({ emailAddress, password });
 
+    const queryResult = queryClient.ensureQueryData(query).then((user) => {
+      // If the stored credentials were not valid, delete the associated cookie
+      if (credentials && !user) {
+        Cookies.remove('sdbc-credentials', {
+          expires: 7,
+          secure: import.meta.env.PROD,
+          sameSite: 'strict',
+        });
+      }
+
+      return user;
+    });
+
     return defer({
-      user: queryClient.ensureQueryData(query),
+      user: queryResult,
     });
   };
 };
