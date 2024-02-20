61c2f029122620183f5e310586f4be548dc4da5b
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-20 09:25:00 -0600

    Add mock API route for course creation

--- src/mocks/api/course.ts
+++ src/mocks/api/course.ts
@@ -1,6 +1,9 @@
 import { http, HttpResponse, RequestHandler } from 'msw';
+import { ValidationError } from 'yup';
 
 import { Course } from '@/entities/course';
+import { courseSchema, CourseUpsertData } from '@/lib/mutations/newCourse.ts';
+import ensureAuth from '@/mocks/api/auth.ts';
 
 import db from './db';
 
@@ -44,6 +47,55 @@ const courseHandlers: RequestHandler[] = [
     } else {
       return HttpResponse.json({ message: 'not found' }, { status: 401 });
     }
+  }),
+
+  http.post('/api/courses', async ({ request }) => {
+    const authResult = ensureAuth(request.headers.get('authorization'));
+
+    if (authResult instanceof Response) return authResult;
+
+    // Extract ID of authenticated user
+    const user = authResult;
+
+    const data = await request.json();
+
+    // Validate course data, throwing a 400 if it fails
+    let result: CourseUpsertData;
+
+    try {
+      result = await courseSchema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const validationErrors = err as ValidationError;
+
+      return HttpResponse.json(validationErrors, { status: 400 });
+    }
+
+    // Create course in mock DB and attach to user
+    const { id, title, description, estimatedTime, materialsNeeded } = db.course.create({
+      ...result,
+      author: user,
+    });
+
+    // Construct course data to return to client
+    const course: Course = {
+      id,
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded,
+      userId: user.id,
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    };
+
+    return HttpResponse.json<Course | { message: string }>(course, {
+      status: 201,
+      headers: {
+        location: `/api/courses/${course.id}`,
+      },
+    });
   }),
 ];
 
