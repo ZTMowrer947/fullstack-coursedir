cea7579751e08527f0f5bf75e1e07edd686ab605
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-20 18:01:09 -0600

    Add course creation mutation

--- src/lib/mutations/newCourse.ts
+++ src/lib/mutations/newCourse.ts
@@ -1,6 +1,8 @@
-import { object, ObjectSchema, string } from 'yup';
+import { object, ObjectSchema, string, ValidationError } from 'yup';
 
 import type { Course } from '@/entities/course.ts';
+import { UserCredentials } from '@/entities/user.ts';
+import { AuthManager } from '@/lib/auth-manager.ts';
 
 export type CourseUpsertData = Pick<Course, 'title' | 'description' | 'estimatedTime' | 'materialsNeeded'>;
 
@@ -10,3 +12,28 @@ export const courseSchema: ObjectSchema<CourseUpsertData> = object({
   estimatedTime: string().nullable().defined(),
   materialsNeeded: string().nullable().defined(),
 });
+
+async function createNewCourse(credentials: UserCredentials, courseData: CourseUpsertData) {
+  const encodedCredentials = `Basic ${AuthManager.encodeCredentials(credentials)}`;
+  const res = await fetch('/api/courses', {
+    method: 'POST',
+    body: JSON.stringify(courseData),
+    headers: {
+      authorization: encodedCredentials,
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+  });
+
+  if (res.ok) {
+    return (await res.json()) as Course;
+  } else if (res.status === 400) {
+    throw new (await res.json())() as ValidationError;
+  } else if (res.status === 401) {
+    throw new Error('Not authorized to create course');
+  } else {
+    throw new Error('Unexpected failure during course creation');
+  }
+}
+
+export default createNewCourse;
