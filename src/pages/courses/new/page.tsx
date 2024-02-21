0fd841527c864d62bf049e106e1c83a18a6a1922
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-02-21 10:12:35 -0600

    Add form error display to new course form
    Add client-side validation

--- src/pages/courses/new/page.tsx
+++ src/pages/courses/new/page.tsx
@@ -1,13 +1,26 @@
-import { FormEventHandler, useCallback, useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormEventHandler, useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Link, useSubmit } from 'react-router-dom';
+import { Link, useActionData, useSubmit } from 'react-router-dom';
 
-import type { CourseUpsertData } from '@/lib/mutations/newCourse.ts';
+import { courseSchema, CourseUpsertData } from '@/lib/mutations/newCourse.ts';
+import addServerErrors from '@/lib/server-validation/add-server-errors.ts';
+import { NewCourseActionResult } from '@/pages/courses/new/action.ts';
 
 export default function NewCoursePage() {
-  const { register, trigger, clearErrors, getValues } = useForm<CourseUpsertData>();
-  const [, setSubmitTimestamp] = useState(-1);
+  const {
+    register,
+    trigger,
+    clearErrors,
+    getValues,
+    setError,
+    formState: { errors },
+  } = useForm<CourseUpsertData>({
+    resolver: yupResolver(courseSchema),
+  });
+  const [submitTimestamp, setSubmitTimestamp] = useState(-1);
   const submit = useSubmit();
+  const actionResult = useActionData() as NewCourseActionResult | undefined;
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = useCallback(
     async (event) => {
@@ -32,27 +45,61 @@ export default function NewCoursePage() {
     [clearErrors, getValues, submit, trigger],
   );
 
+  useEffect(() => {
+    if (!actionResult || actionResult instanceof Response) return;
+
+    if (actionResult.timestamp > submitTimestamp) {
+      addServerErrors(actionResult.err, setError);
+    }
+  }, [actionResult, setError, submitTimestamp]);
+
   return (
     <form method="post" onSubmit={handleSubmit}>
       <div>
-        <input type="text" id="title" placeholder="Course title..." {...register('title')} />
-        <textarea
-          id="description"
-          cols={30}
-          rows={10}
-          placeholder="Course description..."
-          {...register('description')}
-        ></textarea>
+        <div>
+          <input
+            type="text"
+            id="title"
+            placeholder="Course title..."
+            aria-invalid={!!errors.title}
+            {...register('title')}
+          />
+          {errors.title && <p role="alert">{errors.title.message}</p>}
+        </div>
+        <div>
+          <textarea
+            id="description"
+            cols={30}
+            rows={10}
+            placeholder="Course description..."
+            aria-invalid={!!errors.description}
+            {...register('description')}
+          ></textarea>
+          {errors.description && <p role="alert">{errors.description.message}</p>}
+        </div>
       </div>
       <div>
-        <input type="text" id="estimatedTime" placeholder="### Hours" {...register('estimatedTime')} />
-        <textarea
-          id="materialsNeeded"
-          cols={10}
-          rows={10}
-          placeholder="List materials..."
-          {...register('materialsNeeded')}
-        ></textarea>
+        <div>
+          <input
+            type="text"
+            id="estimatedTime"
+            placeholder="### Hours"
+            aria-invalid={!!errors.estimatedTime}
+            {...register('estimatedTime')}
+          />
+          {errors.estimatedTime && <p role="alert">{errors.estimatedTime.message}</p>}
+        </div>
+        <div>
+          <textarea
+            id="materialsNeeded"
+            cols={10}
+            rows={10}
+            placeholder="List materials..."
+            aria-invalid={!!errors.materialsNeeded}
+            {...register('materialsNeeded')}
+          ></textarea>
+          {errors.materialsNeeded && <p role="alert">{errors.materialsNeeded.message}</p>}
+        </div>
       </div>
       <div>
         <button type="submit">Create Course</button>
